2cb42589dde2dbb344ddf672d576dde012631936
jamesjlinden <jamesjlinden@gmail.com>
2020-01-20 13:56:10 -0800

    BinarySearchTree with Insert and Delete. Search will come next.

--- DataStructures/Main.cpp
+++ DataStructures/Main.cpp
@@ -10,6 +10,7 @@
 #include "SinglyLinkedList.hpp"
 #include <string>
 #include <list>
+#include "BinarySearchTree.hpp"
 
 using namespace std;
 
@@ -17,6 +18,5 @@ int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	RunSinglyLinkedListDriver();
-
+	RunBinarySearchTreeDriver();
 }
