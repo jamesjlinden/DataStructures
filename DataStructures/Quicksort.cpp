aebc84dd6391177d73eb5131695c6b263fff683f
jamesjlinden <jamesjlinden@gmail.com>
2020-02-09 15:00:24 -0800

    Quick sort stubs.

---
+++ DataStructures/Quicksort.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "Quicksort.h"
+
+template<class IteratorType>
+inline void Quicksort(IteratorType begin, IteratorType end)
+{
+}
---
+++ DataStructures/Quicksort.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <array>
+
+// We're only performing sorting algorithms over simple data types for educational purposes.
+template<class IteratorType>
+void Quicksort(IteratorType begin, IteratorType end);
+
