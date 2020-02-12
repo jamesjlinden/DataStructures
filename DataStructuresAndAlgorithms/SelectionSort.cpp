c49fa4e561f1b5e79ed25b35961df6d548fe326c
jamesjlinden <jamesjlinden@gmail.com>
2020-02-11 21:46:08 -0800

    Selection sort implementation.

--- DataStructuresAndAlgorithms/SelectionSort.cpp
+++ DataStructuresAndAlgorithms/SelectionSort.cpp
@@ -1,1 +1,35 @@
 #include "pch.h"
+#include "SelectionSort.h"
+#include "ContainerHelpers.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void SelectionSort(vector<int>& a)
+{
+    for (int step = 0; step < a.size(); ++step)
+    {
+        int minIndex = step;
+        for (int i = step + 1; i < a.size(); ++i)
+        {
+            if (a[i] < a[minIndex])
+                minIndex = i;
+        }
+        if (minIndex != step)
+            swap(a[step], a[minIndex]);
+    }
+}
+
+void RunSelectionSortDriver()
+{
+
+    vector<int> sequence1 = { 19, 21, 90, 44, 77, 12 };
+
+    cout << "sequence1 = " << ContainerToString(sequence1.begin(), sequence1.end()) << endl;
+    cout << "SelectionSort() on sequence1..." << endl;
+
+    SelectionSort(sequence1);
+
+    cout << "sequence1 = " << ContainerToString(sequence1.begin(), sequence1.end()) << endl;
+}
