3eba1c3c33d089844321ca005c42c983ccdc368a
jamesjlinden <jamesjlinden@gmail.com>
2020-02-11 21:19:39 -0800

    Stubs for Insertion and Selection sorts.

---
+++ DataStructuresAndAlgorithms/InsertionSort.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include "InsertionSort.h"
+#include "ContainerHelpers.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void InsertionSort(vector<int>& a)
+{
+    if (a.size() <= 1)
+        return;
+
+    for (int i = 1; i < a.size(); ++i)
+    {
+        int currElement = a[i];
+
+        int j = i - 1;
+        while (j >= 0 && currElement < a[j])
+        {
+            a[static_cast<size_t>(j + 1)] = a[j];
+            --j;
+        }
+        a[static_cast<size_t>(j + 1)] = currElement;
+    }
+}
+
+void RunInsertionSortDriver()
+{
+    vector<int> sequence1 = { 12, 8, 92, 81, 4, 59 };
+
+    cout << "sequence1 = " << ContainerToString(sequence1.begin(), sequence1.end()) << endl;
+    cout << "InsertionSort() on sequence1..." << endl;
+
+    InsertionSort(sequence1);
+
+    cout << "sequence1 = " << ContainerToString(sequence1.begin(), sequence1.end()) << endl;
+}
